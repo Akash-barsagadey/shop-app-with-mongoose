7be85f122929fc9c69bb2d4bebcc158309d092d9
Akash Barsagadey <akash_udaram.barsagadey@syngenta.com>
2020-04-03 18:09:28 +0530

    Add session use connect-mongodb-session to store

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://akashbarsagadey:akash12345@cluster0-l7dfp.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,6 +27,14 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
   User.findById('5e833eee38cbe913786e4213')
@@ -35,10 +52,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://akashbarsagadey:akash12345@cluster0-l7dfp.mongodb.net/shop?retryWrites=true&w=majority',
-    { useUnifiedTopology: true, useNewUrlParser: true }
-  )
+  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
